355d3b9000abe019038202dfff7b7a9f39fc6ca6
Adithyan.A <deepaadithyan56@gmail.com>
2021-05-24 13:55:19 +0530

    Project completed

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="navbar-fixed">
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#!" className="brand-logo">TODO APP REACT JS</a>
+                    {/*<ul className="right hide-on-med-and-down">*/}
+                    {/*<li><a href="sass.html">Sass</a></li>*/}
+                    {/*<li><a href="badges.html">Components</a></li>*/}
+                    {/*</ul>*/}
+                </div>
+            </nav>
+        </header>
+    )
+};
+
+export default Header
